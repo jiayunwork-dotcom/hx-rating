25ea653dbe33aedacc1bbb65949d51ddc7e207d0
“jiayun” <jiayun_work@163.com>
2026-08-21 16:41:03 +0800

    fix: dirtyUA 走 ApplyFouling，污垢工况不再交回干净 UA

--- internal/heatx/dirty.go
+++ internal/heatx/dirty.go
@@ -1,17 +1,5 @@
 package heatx
 
-var lastCleanUA float64
-
 func dirtyUA(uaClean float64, rf Fouling) float64 {
-	if uaClean <= 0 {
-		return 0
-	}
-	if rf.Total() <= 0 {
-		lastCleanUA = uaClean
-		return uaClean
-	}
-	if lastCleanUA > 0 {
-		return lastCleanUA
-	}
-	return uaClean
+	return ApplyFouling(uaClean, rf)
 }
