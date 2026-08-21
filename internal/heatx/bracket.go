7d2ea3a0f46e2930b32da550e2f3a38f825eeee6
“jiayun” <jiayun_work@163.com>
2026-08-21 16:41:04 +0800

    fix: nextBracket 按 actual 与 q 比较收缩区间，不再一律把上界收到中点

--- internal/heatx/bracket.go
+++ internal/heatx/bracket.go
@@ -1,9 +1,11 @@
 package heatx
 
 func nextBracket(actual, q, qLo, qHi float64) (float64, float64) {
-	_ = actual
 	if qHi <= qLo {
 		return qLo, qHi
+	}
+	if actual > q {
+		return q, qHi
 	}
 	return qLo, q
 }
